5e42440a345f9ac8e76f6d82dc1446a17e374c0e
ing.dacastillop <ing.dacastillop@gmail.com>
2026-03-30 13:52:09 -0500

    :sparkles: Implement object Signals by encapsulated keys by reactive field Value

--- src/index.ts
+++ src/index.ts
@@ -37,15 +37,46 @@ export abstract class AbstractStore<T extends LiteralObject> {
   abstract reset(): void;
 }
 
+export type StoreSignals<T extends LiteralObject> = {
+  readonly [K in keyof T]-?: Signal<T[K]>;
+};
+
 export class Store<T extends LiteralObject> implements AbstractStore<T> {
+  protected readonly _signals: StoreSignals<T>;
+
   private readonly _state: State<T>;
+
+  private readonly _cache: Map<keyof T, Signal<any>> = new Map();
 
   constructor(value: T) {
     this._state = new State(value);
+
+    this._signals = new Proxy({} as StoreSignals<T>, {
+      get: (_target, prop: string | symbol) => {
+        if (typeof prop !== 'string') {
+          return undefined;
+        }
+
+        const key = prop as keyof T;
+
+        if (!this._cache.has(key)) {
+          this._cache.set(
+            key,
+            computed(() => this._state.value()[key])
+          );
+        }
+
+        return this._cache.get(key);
+      }
+    });
   }
 
   public get value(): Signal<Readonly<T>> {
     return this._state.value;
+  }
+
+  public get signals(): StoreSignals<T> {
+    return this._signals;
   }
 
   public reset(): void {
